ddb1ab2e1e29e26f339c5ac4c9627a49d0a70897
JoaoDobrowolski <joaodobrowolski@outlook.com>
2022-08-06 01:19:06 -0300

    grupo16 requisito5

---
+++ src/pages/Game.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <>
+        Game
+        <Header />
+      </>
+    );
+  }
+}
+
+export default Game;
