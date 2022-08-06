edb81a9fedbef88a9ac783c72a1d0dcd33501240
Silverio Moraes <silverstone191013@gmail.com>
2022-08-05 22:49:43 -0300

    requesito-2 correção  ✅

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
 
-
 function App() {
   return (
     // <div className="App">
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import { fetchToken } from '../services/FetchAPI';
-import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   constructor() {
