c99259412951d04db96eff57d5668992db54bdf4
Silverio Moraes <silverstone191013@gmail.com>
2022-08-09 17:29:46 -0300

    group16-req-4 correção  ✅

--- src/tests/helpers/LoadingScreen.test.js
+++ src/tests/helpers/LoadingScreen.test.js
@@ -1,34 +1,47 @@
 import React from "react";
 import { screen } from "@testing-library/react";
 import App from "../../App";
+import {mapDispatchToProps} from '../../pages/Login'
 import userEvent from "@testing-library/user-event";
 import renderWithRouterAndRedux from "./renderWithRouterAndRedux";
 
 
 describe('Pagina de login', () => {
-  it('Botão Play', () => {
-    renderWithRouterAndRedux(<App />);
+  it('Botão Play', async () => {
+    const { history} = renderWithRouterAndRedux(<App />);
 
     const campName = screen.getByPlaceholderText('Nome');
     const campEmail = screen.getByPlaceholderText('Email');
     const btnPlay = screen.getByRole('button', {  name: /play/i});
 
+    expect(campEmail).toBeDefined();
+    expect(campName).toBeDefined();
+    expect(btnPlay).toBeDefined();
+
     expect(btnPlay.disabled).toBe(true);
 
     userEvent.type(campName, 'nome');
-    userEvent.type(campEmail, 'email');
+    expect(campName).toHaveValue('nome')
+
+    userEvent.type(campEmail, 'alguem@alguem.com');
+    expect(campEmail).toHaveValue('alguem@alguem.com')
 
     expect(btnPlay.disabled).toBe(false);
 
     userEvent.click(btnPlay);
+    await screen.findByText(/categoria/i)
+
+    expect(history.location.pathname).toBe('/game');
+
   });
   it('Botão de Configurações', () => {
-    renderWithRouterAndRedux(<App />);
+    const { history} = renderWithRouterAndRedux(<App />);
 
     const btnConfig = screen.getByRole('button', {  name: /configurações/i});
+    expect(btnConfig).toBeDefined();
     userEvent.click(btnConfig);
 
-    const screenConfig = screen.getByText(/configurações/i);
-    expect(screenConfig).toBeInTheDocument();
+    expect(history.location.pathname).toBe('/settings');
+
   });
 });
