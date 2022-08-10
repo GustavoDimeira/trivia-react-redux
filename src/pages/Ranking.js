4827b049d035d20b32817e04c26d6ebb36c2a608
JoaoDobrowolski <joaodobrowolski@outlook.com>
2022-08-10 15:48:09 -0300

    grupo 16 req 16 e 18

---
+++ src/pages/Ranking.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Ranking extends React.Component {
+  render() {
+    const { history } = this.props;
+    return (
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          data-testid="btn-go-home"
+          type="button"
+          onClick={ () => history.push('/') }
+        >
+          Play Again
+        </button>
+      </div>
+    );
+  }
+}
+
+Ranking.propTypes = {
+  history: PropTypes.shape().isRequired,
+};
+
+export default Ranking;
